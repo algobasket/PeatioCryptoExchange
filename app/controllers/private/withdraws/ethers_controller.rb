f00ccb96613d57e9df2a1667033f98196a1480d1
Algobasket <algobasket@gmail.com>
2019-04-09 07:02:04 +0530

    Repairing Repo

--- app/controllers/private/withdraws/ethers_controller.rb
+++
@@ -1,7 +0,0 @@
-module Private
-  module Withdraws
-    class EthersController < ::Private::Withdraws::BaseController
-      include ::Withdraws::Withdrawable
-    end
-  end
-end
--- app/models/withdraws/ether.rb
+++
@@ -1,7 +0,0 @@
-module Withdraws
-  class Ether < ::Withdraw
-    include ::AasmAbsolutely
-    include ::Withdraws::Coinable
-    include ::FundSourceable
-  end
-end
